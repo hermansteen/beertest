0e9f694c83c7ec7fc6f70f822dfbe4112e7f1a1c
Herman Steén <herman.a.steen@gmail.com>
2023-01-10 14:57:03 +0100

    scraper and app changed to only have one database file

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ import { useEffect, useState } from 'react'
 import * as d3 from 'd3'
 import Input from './components/Input'
 import BeerCard from './components/BeerCard'
-import wineData from './utils/wine.csv'
+//import wineData from './utils/wine.csv'
 import productLinks from './utils/links.csv'
 import Slider from './components/Slider.js';
-import beerData from './utils/products.csv'
+import productData from './utils/testing.csv'
 import Instructions from './components/Instructions'
 
-const beerDB = beerData;
-const wineDB = wineData;
+const productDB = productData;
+//const wineDB = wineData;
 
 //Temp bool
 let bBeer = true;
@@ -41,10 +41,7 @@ const beerToString = beer => {
 }
 
 const loadProducts = async () => {
-  await d3.csv(beerDB, (d) => {
-    allProducts.push(d)
-  })
-  await d3.csv(wineDB, (d) => {
+  await d3.csv(productDB, (d) => {
     allProducts.push(d)
   })
   allProducts = removeDuplicates(allProducts)
